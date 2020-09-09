59b1a0aa59a48be46fa4655f403873aee0192c41
郑笑飞 <kristen@electracharger.com>
2020-09-09 10:57:28 +0800

    add:home page uncomplete

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -7,5 +7,36 @@
  * @FilePath: /fe-website/src/pages/home/index.vue
 -->
 <template>
-  <div>首页</div>
+  <div>
+    <div class="home-top">
+      <img src="@/assets/home_bg.png">
+      <p>Get powered up anytime, anywhere</p>
+      <p>Available to charger 99% of all smartphone</p>
+      <div>
+        <img 
+          class="download-style" 
+          @click="handleDownLoad('ios')" 
+          src="@/assets/download_ios.png"/>
+        <img 
+          class="download-style" 
+          @click="handleDownLoad('android')" 
+          src="@/assets/download_android.png"/>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.download-style {
+  width: 0.7rem;
+  height: 0.18rem;
+  margin-top: 0.04rem;
+  margin-right: 0.05rem;
+}
+</style>
